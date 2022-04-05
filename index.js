960c237391e49635bbe6e37ad68b1d6394e0f4c8
Andy Quach <quachan888@gmail.com>
2022-04-05 16:54:41 -0400

    Mongoose Part 3

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 connect.then(() => {
@@ -18,10 +19,34 @@ connect.then(() => {
     })
         .then((campsite) => {
             console.log('Add succedded', campsite);
-            return Campsite.find();
+            // Add comments to campsite
+            return Campsite.findByIdAndUpdate(
+                campsite._id,
+                {
+                    $set: { description: 'Updated Test Document' }
+                },
+                {
+                    new: true
+                }
+            );
         })
         .then((campsite) => {
-            console.log('Founded: ', campsite);
+            console.log('After update: ', campsite);
+            campsite.comments.push({
+                rating: 5,
+                text: 'What a beautiful view!',
+                author: 'Andy'
+            });
+            campsite.comments.push({
+                rating: 3,
+                text: 'A lot of mosquitos!',
+                author: 'Sandy'
+            });
+
+            return campsite.save();
+        })
+        .then((campsite) => {
+            console.log('Founded updated campsite: ', campsite);
             return Campsite.deleteMany();
         })
         .then(() => {
